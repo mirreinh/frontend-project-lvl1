c40d80ce4d270c7e0d8f537a8f96eec789f3197d
mirreinh <mirreinhh@gmail.com>
2021-01-28 21:40:44 +0300

    create new game – progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,25 @@
+import getRandomInt from '../utils.js';
+import runGame from '../index.js';
+
+const rules = 'What number is missing in the progression?';
+
+const missingNumberInArray = getRandomInt(0, 9);
+
+const getQuestionAndCorrectAnswer = () => {
+  let progressionMember = getRandomInt(1, 100);
+  const progressionStep = getRandomInt(1, 10);
+
+  let index = 0;
+  const arr = [progressionMember];
+  const progressionLength = 10;
+
+  for (index; index < progressionLength - 1; index += 1) {
+    progressionMember += progressionStep;
+    arr.push(progressionMember);
+  }
+  const correctAnswer = arr[missingNumberInArray];
+  arr[missingNumberInArray] = '..';
+  return [arr.join(', '), correctAnswer];
+};
+
+export default () => runGame(rules, getQuestionAndCorrectAnswer);
