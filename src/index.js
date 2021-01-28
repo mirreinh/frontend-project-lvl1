6b1a3c7b6e7ef9298f4a3f62989c1ee0d3ef7ece
mirreinh <mirreinhh@gmail.com>
2021-01-28 22:08:22 +0300

    fix engine [2]

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ const runGame = (rules, getQuestionAndAnswer) => {
     } else {
       console.log(`'${answer}' is wrong answer ;(`);
       console.log(`Correct answer was '${trueAnswer}'.`);
-      console.log("Let's try again!");
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
-  console.log(`Congratulations, ${userName}, you won!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default runGame;
